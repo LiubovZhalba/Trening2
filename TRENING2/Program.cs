f2441bd2e3bfaaae94f303d01477e38ffb5fc095
Liubov <liubov.zhalba@yandex.ru>
2023-02-12 19:19:48 +1000

    Решение ДЗ к 3-му Семинару

--- TRENING2/Program.cs
+++ TRENING2/Program.cs
@@ -1,6 +1,15 @@
-﻿
+﻿int PalinDrom(int number)
+{
+    int des1 = number / 10000;
+    int ed1 = number % 1000;
+    int des2 = number / 100;
+    int ed2 = number % 10;
 
-Console.Write("Введите число: ");
+    int result = (des1 * 10 + ed1) / (des2 * 10 + ed2);
+    return result;  
+}
+
+/*Console.Write("Введите число: ");
 int num = Convert.ToInt32(Console.ReadLine());
 
 int[]array =  {1,2,3,4,25,65,89};
--- TRENING2/Yrok3/Program.cs
+++ TRENING2/Yrok3/Program.cs
@@ -1,2 +1,71 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
+// Задача 1: Программа принимает на вход 5-ти значное число и сообщает палинтром это или нет
+/*int PalinDrom(int number)
+{
+    int des1 = number / 10000;
+    int ed1 = number / 1000 % 10;
+    int des2 = number / 10 % 10;
+    int ed2 = number % 10;
+
+    int result = (des1 + ed1) - (des2 + ed2);
+    return result;  
+}
+
+Console.Write("Введите 5-ти знач.число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int newnum = PalinDrom(num);
+{
+    if(newnum == 0)
+    {
+        Console.WriteLine("Палиндром: ");
+    }
+    else
+    {
+        Console.WriteLine("Не палиндром: ");
+    }
+}*/
+// Задача 2: Программа принимает координаты 2-х точек и находит расстояние между ними в 3-D пространстве
+
+double DlinaOtr(double x1, double y1, double z1, double x2, double y2, double z2)
+{
+    double L1 = x2 - x1;
+    double L2 = y2 - y1;
+    double L3 = z2 - z1;
+    double result = Math.Sqrt(Math.Pow(L1,2) + Math.Pow(L2,2) + Math.Pow(L3,2));
+    return result;
+}
+Console.Write("Введите координаты х точки А: ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координаты y точки А: ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координаты z точки А: ");
+double z1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координаты х точки В: ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координаты y точки В: ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координаты z точки В: ");
+double z2 = Convert.ToDouble(Console.ReadLine());
+
+double Dlina = DlinaOtr(x1, y1, z1, x2, y2, z2);
+
+Console.WriteLine(Math.Round(Dlina));
+
+// Задача 3: Программа принимает на вход N и выдает куб чисел от 1 до N
+
+/*void PrintCub(int N)
+{
+    int i = 1;
+    while(i <= N)
+    {
+        Console.Write(i * i * i + " ");
+        i = i + 1;
+    }
+}
+Console.Write("Введите число элементов: ");
+int N = Convert.ToInt32(Console.ReadLine());
+PrintCub(N);*/
+
+
