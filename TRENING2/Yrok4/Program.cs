09c54a6911bd668dae793646ed1b01e8049b7871
Liubov <liubov.zhalba@yandex.ru>
2023-02-17 21:49:02 +1000

    ДЗ к 4 Уроку

---
+++ TRENING2/Yrok4/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+// ДЗ урок 4 Задача 1: программа принимает на вход 2 числа (а, в) и возвдит число а в степень в
+/*double Stepen(double a, double b)
+{
+    double result = Math.Pow(a,b);  
+    return result;  
+}
+Console.Write("Введите первую цифру: ");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите вторую цифру: ");
+double b = Convert.ToDouble(Console.ReadLine());
+
+double newnumber = Stepen(a, b);
+
+Console.WriteLine(newnumber);*/
+
+// ДЗ 4 задача 2: программа принимает на вход число и выдает сумму чисел
+
+/*Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int result = ((num / 100) + (num / 10 % 10) + (num % 10));
+Console.WriteLine(result);*/
+
+
+//ДЗ 4 Задача 3 Программа задает массив из m элементов и выводит их на экран
+
+/*void PrintMassiv(int N)
+{
+    int index = 0;
+    while(index <= N)
+    {
+        Console.Write(index + " ");
+        index++;
+    }
+}
+Console.Write( "Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+PrintMassiv(N);*/
