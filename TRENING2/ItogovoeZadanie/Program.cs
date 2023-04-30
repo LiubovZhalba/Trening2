e53d9182ecdfbc11b74c2da634beb46091944405
Liubov <liubov.zhalba@yandex.ru>
2023-05-01 09:21:35 +1000

    Добавлен код к Итоговому заданию

---
+++ TRENING2/ItogovoeZadanie/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*Console.WriteLine("Hello, World!");*/
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    } 
+    Console.WriteLine();
+}
+
+Console.Write("Введите количество массивов ");
+int size = Convert.ToInt32(Console.ReadLine());
+string[] arrayStrings = new string[size];
+int len = 3;
+int pos = 0;
+for (int i = 0; i < size; i++)
+{
+    Console.WriteLine($" Введите элементы массива: ");
+    string element = Convert.ToString(Console.ReadLine());
+    if (element.Length <= len)
+    {
+        arrayStrings[pos] = element;
+        pos ++;
+    }
+}
